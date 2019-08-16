cf438f45d292d22fb3ad1a9cef50e00514e6d99d
Wes <wdemoney@gmail.com>
2019-08-16 11:02:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# zn-backend-upload-to-form-record-field
+
+Upload a file to a form record's file upload field.
+
+## Installation
+
+```bash
+npm i @zenginehq/backend-upload-to-form-record-field --save
+```
+
+## Usage
+
+```js
+const $uploadToFormRecordField = require('@zenginehq/backend-upload-to-form-record-field');
+
+exports.run = function(eventData) {
+
+    // ...
+
+    $uploadToFormRecordField({
+		namespace: eventData.request.params.pluginNamespace,
+		formId: formId,
+		recordId: recordId,
+		fieldId: uploadFieldId,
+		filePath: 'http://.../somefile.pdf',
+		fileName: 'upload-filename.pdf'
+    }).then(function(result) {
+        eventData.response.status(200).send(result);
+    }).catch(function(err) {
+        eventData.response.status(500).send();
+    });
+
+};
+```
+
+### Options Reference
+
+Either `namespace` or `clientId` are required. All other options are required.
+
+- **namespace(_string_)**: Plugin namespace to fetch plugin and client id. Plugin must be offline enabled.
+- **clientId(_number_)**: API client id.
+- **formId(_number_)**: Form id.
+- **recordId(_number_)**: Existing record id.
+- **fieldId(_number_)**: Upload form field id.
+- **filePath(_string_)**: Local file path or absolute URL to file.
+- **fileName(_string_)**: Upload filename. Filename extension will be used to determine content-type.
---
+++ index.js
@@ -0,0 +1,100 @@
+'use strict';
+
+const $getPluginClientId = require('@zenginehq/backend-get-plugin/get-client-id');
+
+const downloadFile = require('./lib/download-file');
+
+const uploadToFormRecord = require('./lib/upload-to-form-record');
+
+const deleteLocalFile = require('./lib/delete-local-file');
+
+/**
+ * Upload File to Form Record Field
+ *
+ * @param {Object} options
+ * @param {string} options.baseUrl - API Base URL
+ * @param {string} options.namespace - Plugin Namespace
+ * @param {number} options.clientId - OAuth Client Id
+ * @param {number} options.formId
+ * @param {number} options.recordId
+ * @param {number} options.fieldId
+ * @param {number} options.filePath - Local Path or Remote Url to File
+ * @param {number} options.fileName - Upload Filename
+ */
+async function uploadToFormRecordField(options) {
+
+	const {
+		baseUrl = 'https://api.zenginehq.com/v1',
+		namespace,
+		formId,
+		recordId,
+		fieldId,
+		filePath,
+		fileName
+	} = options;
+
+	let {
+		clientId
+	} = options;
+
+	if ((!clientId && !namespace) || !formId || !recordId || !fieldId || !filePath || !fileName) {
+		throw new Error('Missing required params');
+	}
+
+	// Get Client Id from Namespace
+	if (!clientId) {
+		clientId = await $getPluginClientId(namespace);
+	}
+
+	if (!clientId) {
+		throw new Error('Plugin must be `offline` mode and have an OAuth 2 client id');
+	}
+
+	let localPath = filePath;
+
+	// Download Remote Url to Local File
+	if (filePath.substring(0, 4) === 'http') {
+		localPath = await downloadFile(filePath);
+	}
+
+	// Cleanup Local File
+	function cleanup() {
+
+		if (localPath !== filePath) {
+			return deleteLocalFile(localPath);
+		}
+
+		return true;
+
+	}
+
+	try {
+
+		// Upload File and Save to Record
+		await uploadToFormRecord({
+			baseUrl,
+			clientId,
+			formId,
+			recordId,
+			fieldId,
+			filePath: localPath,
+			fileName
+		});
+
+	}
+	catch (e) {
+
+		// Still Cleanup if Upload Fails
+		cleanup();
+
+		throw e;
+
+	}
+
+	cleanup();
+
+	return true;
+
+}
+
+module.exports = uploadToFormRecordField;
